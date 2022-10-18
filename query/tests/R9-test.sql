7c1f61a51759b6d430d090be911307ed98a3084e
Yunt Flare <peterliangap@outlook.com>
2022-10-18 19:43:25 -0400

    update test and test outputs

--- query/tests/R9-test.sql
+++ query/tests/R9-test.sql
@@ -1,17 +1,20 @@
--- Test UPDATE_product_rating_ON_INSERT trigger
+-- 1. Test UPDATE_product_rating_ON_INSERT trigger
 SELECT * FROM comment WHERE product_id=100;
 SELECT * FROM product WHERE product_id=100;
 INSERT INTO Comment VALUES(200, 5, now(), "First time, nice", 1, 100);
 SELECT * FROM product WHERE product_id=100;
--- Test UPDATE_product_rating_ON_DELETE trigger
+
+-- 2. Test UPDATE_product_rating_ON_DELETE trigger
 SELECT * FROM comment WHERE product_id=100;
 SELECT * FROM product WHERE product_id=100;
 DELETE FROM Comment WHERE comment_id = 200;
 SELECT * FROM product WHERE product_id=100;
--- Test UPDATE_product_rating_ON_UPDATE trigger
+
+-- 3. Test UPDATE_product_rating_ON_UPDATE trigger
 SELECT * FROM comment WHERE product_id=100;
 SELECT * FROM product WHERE product_id=100;
 UPDATE Comment SET rating=0 WHERE comment_id=106;
 SELECT * FROM product WHERE product_id=100;
--- Test get product rating by id
+
+-- 4. Test get product rating by id
 SELECT product_rating FROM Product WHERE product_id=100;

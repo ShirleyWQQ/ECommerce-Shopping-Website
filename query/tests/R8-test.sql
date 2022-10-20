7db706085af4a57e23c93c27199a89d3ef0ee1cd
Peter Liang <peter.liang@uwaterloo.ca>
2022-10-20 19:41:24 -0400

    Update tests based on new feature

--- query/tests/R8-test.sql
+++ query/tests/R8-test.sql
@@ -1,21 +1,11 @@
--- 1. Insert comments
-insert into Comment values
-(201, 3.5, '2020-10-14 10:20:00', "very good", 100, 308);
-insert into Comment values
-(202, 4.5, "2021-10-11 10:10:10", "I love it", 200, 308);
-select * from Comment;
-
--- 2. Update a comment
-update Comment set
-	content = 'update',
-    updated_time = now()
-    where comment_id = 201;
-select * from Comment;
-
--- 3. Remove a comment
-delete from Comment
-  where comment_id = 202;
-select * from Comment;
-
--- 4. Get all comments by product_id
-select * from Comment where product_id = 100;
+/* Running from sample data */
+/* Delete Product by product_id and delete cascade its comment*/
+SELECT * FROM Product WHERE product_id = 100;
+SELECT * FROM Comment WHERE product_id = 100;
+DELETE FROM Product WHERE product_id = 100;
+SELECT * FROM Product WHERE product_id = 100;
+SELECT * FROM Comment WHERE product_id = 100;
+/* Delete Comment by comment_id */
+SELECT * FROM Comment;
+DELETE FROM Comment WHERE comment_id = 102;
+SELECT * FROM Comment;

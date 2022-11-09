1d815ad2ae410ba21338ad8bda7a729d7f0afb2a
Yunt <PeterLiangAP@Outlook.com>
2022-11-09 16:10:27 -0500

    functional component

--- client/src/components/ServerCheck.js
+++ client/src/components/ServerCheck.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Axios from "axios";
 import Button from "react-bootstrap/Button"
 
+
+export default function ServerCheck() {
+  const [serverStatus, setStatus] = useState("Unknown");
+  const getServerStatus = () => {
+    Axios.get("http://localhost:3001/status")
+      .then(res => {
+        setStatus(res.data);
+      })
+      .catch(err => {
+        setStatus("Server is not running");
+      });
+  }
+  return (
+    <div style={{ padding: 10, backgroundColor: "LightGreen" }}>
+      <Button onClick={getServerStatus}>Check Server</Button><br />
+      <i><b>{serverStatus}</b></i><br />
+    </div>
+  );
+}
+/*
 export default class ServerCheck extends React.Component {
   constructor(props) {
     super(props);
@@ -28,3 +48,4 @@ export default class ServerCheck extends React.Component {
     );
   }
 }
+*/

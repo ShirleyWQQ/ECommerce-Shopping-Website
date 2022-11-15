57205c8f2b5498e2a1b618ad248c5074f6ea0369
Yunt Flare <peterliangap@outlook.com>
2022-11-14 21:25:28 -0500

    product list and its css

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ProductCard from "./ProductCard";
+import "./css/ProductList.css";
 
 // props: data(product[])
 export default function ProductList(props) {
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
+    <div class="flex-container">
       {props.data.map(item => (
         <ProductCard
           name={item.product_name}

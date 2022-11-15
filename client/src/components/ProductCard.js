ee9e7990c9e7fce05ef0b5c02b7e1cd3d9793f0d
c684wang@uwaterloo.ca <c684wang@uwaterloo.ca>
2022-11-14 22:22:17 -0500

    updated card text look

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Button from "react-bootstrap/Button"
+import "./css/ProductCard.css";
 // props: name price description
 export default function ProductCard(props) {
   return (
     <div className="card" style={{width: '18rem'}}>
       <img className="card-img-top" src={props.image} alt="Product" />
       <div className="card-body">
-        <h5 className="card-title">{props.name}</h5>
-        <h5 className="card-title2">{props.price}</h5>
-        <p className="card-text">{props.description}</p>
+        <h5 className="card-title" id="para0">{props.name}</h5>
+        <h5 className="card-title2">${props.price}</h5>
+        <p className="card-text" id="para">{props.description}</p>
         <Button>+</Button>
       </div>
     </div>

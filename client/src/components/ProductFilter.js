53319156104d3d43b2f35a643381a62b96563e59
Yunt Flare <peterliangap@outlook.com>
2022-11-16 17:29:56 -0500

    update navigation

--- client/src/components/ProductFilter.js
+++ client/src/components/ProductFilter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import StarIcon from "../images/StarIcon";
--- client/src/pages/ExamplePage.js
+++ client/src/pages/ExamplePage.js
@@ -1,10 +1,8 @@
 import ServerCheck from "../components/examples/ServerCheck";
-import Counter from "../components/examples/Counter";
 export default function ExamplePage() {
   return (
     <div>
       <ServerCheck />
-      <Counter />
     </div>
   )
 }

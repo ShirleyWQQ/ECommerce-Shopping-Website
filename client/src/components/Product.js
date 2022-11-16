9cb608dc08648a1bc1a25a0236f78df81d12bf4b
c684wang@uwaterloo.ca <c684wang@uwaterloo.ca>
2022-11-16 11:47:27 -0500

    product display updated

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -4,12 +4,18 @@ import "./css/ProductCard.css";
 // props: name price description rating comment
 export default function Product(props) {
   return (
-    <div className="product" style={{width: '18rem'}}>
-      <img className="img-top" src={props.image} alt="no image available" />
-        <h5 className="name">{props.name}</h5>
-        <p className="description">{props.description}</p>
-        <h5 className="price">${props.price}</h5>
-        <h5 className="rating">{props.rating}</h5>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <img className="img-top" src={props.image} alt="no image available" />
+        </div>
+        <div className="col">
+          <h5 className="name">{props.name}</h5>
+          <h5 className="price">${props.price}</h5>
+          <h5 className="rating">{props.rating}</h5>
+          <p className="description">{props.description}</p>
+        </div>
+      </div>
     </div>
   );
 }

03bb31b4158f28445b41e24e5a4a74bacad75a89
c684wang@uwaterloo.ca <c684wang@uwaterloo.ca>
2022-11-16 11:32:25 -0500

    made comment look better and added a style sheet

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,13 +1,14 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
+import "./css/Comment.css";
 // props: name price description
 export default function Comment(props) {
   return (
-    <div className="comment" style={{width: '18rem'}}>
-        <h5 className="username">{props.user_name}</h5>
-        <h5 className="rating">{props.rating}</h5>
-        <h5 className="updated_time">{props.updated_time}</h5>
-        <p className="content">{props.content}</p>
+    <div className = "row">
+        <div className="col">{props.user_name}</div>
+        <div className="col">{props.rating}</div>
+        <div className="col">{props.updated_time}</div>
+        <div className="col">{props.content}</div>
     </div>
   );
 }

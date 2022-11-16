c063d8bb1ac93447bb3bc31de634e3f2d2c0de95
Yunt Flare <peterliangap@outlook.com>
2022-11-16 10:41:52 -0500

    update comment

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -4,9 +4,9 @@ import React from "react";
 export default function Comment(props) {
   return (
     <div className="comment" style={{width: '18rem'}}>
-        <h5 className="comment_id">${props.comment_id}</h5>
-        <h5 className="rating">${props.rating}</h5>
-        <h5 className="updated_time">${props.updated_time}</h5>
+        <h5 className="username">{props.user_name}</h5>
+        <h5 className="rating">{props.rating}</h5>
+        <h5 className="updated_time">{props.updated_time}</h5>
         <p className="content">{props.content}</p>
     </div>
   );

c995ba642a352610607b0f486e7d3a7dfd3ed5a6
Yunt Flare <peterliangap@outlook.com>
2022-11-16 10:37:29 -0500

    takeout navgation to app level

--- client/src/pages/ExamplePage.js
+++ client/src/pages/ExamplePage.js
@@ -1,10 +1,8 @@
 import ServerCheck from "../components/examples/ServerCheck";
 import Counter from "../components/examples/Counter";
-import Navigation from "../components/examples/Navigation";
 export default function ExamplePage() {
   return (
     <div>
-      <Navigation />
       <ServerCheck />
       <Counter />
     </div>

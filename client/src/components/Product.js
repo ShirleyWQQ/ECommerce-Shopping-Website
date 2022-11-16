f21e400608e07ceb4401074a310a38cfd789773c
Yunt Flare <peterliangap@outlook.com>
2022-11-16 09:53:49 -0500

    prodcut page retrive data

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -3,7 +3,7 @@ import React from "react";
 import Button from "react-bootstrap/Button"
 import "./css/ProductCard.css";
 // props: name price description
-export default function ProductCard(props) {
+export default function Product(props) {
   return (
     <div className="product" style={{width: '18rem'}}>
       <img className="img-top" src={props.image} alt="no image available" />
--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,9 +1,25 @@
-import "./ProductPage.css"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Product from "../components/Product";
+import api from "../lib/api";
 
 export default function ProductPage() {
+  const { product_id } = useParams();
+  const [product, setProduct] = useState({ found: false });
+  useEffect(() => {
+    api.getProduct(product_id, setProduct);
+  }, [product_id, setProduct]);
   return (
     <div>
-      Product Page placeholder
+      {product
+        ? <Product
+          name={product.product_name}
+          price={product.price}
+          description={product.description}
+          image={product.picture_source}
+          comments />
+        : <h1>Product Not Found</h1>
+      }
     </div>
   )
 }

352cbc52f989467e16d508e31118d13f4815b7fb
c684wang@uwaterloo.ca <c684wang@uwaterloo.ca>
2022-11-26 16:50:46 -0500

    made car look better

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -7,30 +7,30 @@ import StarIcon from "../images/StarIcon";
 function PrintStars(props) {
   const num = parseFloat(props.number);
   if (num > 4) {
-    return <div><StarIcon /><StarIcon /><StarIcon /><StarIcon /><StarIcon /></div>
+    return <div id="rating">{props.number}<StarIcon /><StarIcon /><StarIcon /><StarIcon /><StarIcon /></div>
   } else if (num > 3) {
-    return <div><StarIcon /><StarIcon /><StarIcon /><StarIcon /></div>
+    return <div id="rating">{props.number}<StarIcon /><StarIcon /><StarIcon /><StarIcon /></div>
   } else if (num > 2) {
-    return <div><StarIcon /><StarIcon /><StarIcon /></div>
+    return <div id="rating">{props.number}<StarIcon /><StarIcon /><StarIcon /></div>
   }
-  return <StarIcon />
+  return <div id="rating">{props.number}<StarIcon /></div>
 }
 // props: name price description
 export default function ProductCard(props) {
   const navigate = useNavigate();
   return (
     <div className="card" style={{width: '18rem'}}>
-      <img className="card-img-top" src={props.image} alt="no image available" />
+      <div class="container">
+        <img className="card-img-top" src={props.image} alt="no image available" />
+        <Button classname="btn">+</Button>
+      </div>
       <div className="card-body">
+        <h5 className="price" id="price">${props.price}</h5>
         <h5 className="card-title" id="para0" onClick={() => {navigate(`/product/${props.product_id}`)}}>{props.name}</h5>
         <p className="card-text" id="para">{props.description}</p>
-        <h5 className="card-text2">${props.price}</h5>
-        <h5 className="position-absolute bottom-0 start-0">
-          {props.rating}<PrintStars number={props.rating} />
+        <h5 className="rating">
+          <PrintStars number={props.rating} />
         </h5>
-        <div className="position-absolute bottom-0 end-0">
-          <Button>+</Button>
-        </div>
       </div>
     </div>
   );

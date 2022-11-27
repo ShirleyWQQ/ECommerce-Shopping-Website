15e9cfa585c2c36a9390f9b0470acdbd06eb4e94
c684wang@uwaterloo.ca <c684wang@uwaterloo.ca>
2022-11-27 11:06:33 -0500

    shopping cart prototype

---
+++ client/src/components/ShoppingCart.js
@@ -0,0 +1,19 @@
+// props: image, productName, price, quantity
+export default function ShoppingCart(props) {
+    return (
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <img className="img-top" src={props.image} alt="no image available" />
+            </div>
+            <div className="col">
+              <h5 className="productName">{props.productName}</h5>
+              <h5 className="quantity">{props.quantity}</h5>
+            </div>
+            <div className="col">
+              <h5 className="price">${props.price}</h5>
+            </div>
+          </div>
+        </div>
+      );
+}

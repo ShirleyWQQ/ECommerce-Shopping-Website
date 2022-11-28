cb62173152d53fac2c6cde801bbf5d2659141839
c684wang@uwaterloo.ca <c684wang@uwaterloo.ca>
2022-11-28 16:02:28 -0500

    updated the file

--- query/setup/2b_Index.sql
+++ query/setup/2b_Index.sql
@@ -1,6 +1,13 @@
 -- create indexes
 
--- R6:
+-- R8:
 create index idx_pid on product(product_id);
 create index idx_cid on comment(comment_id);
 create index idx_pid_comment on comment(product_id);
+
+-- R9:
+-- N/A, since we already created indexes for product(product_id) for feature R8
+
+-- R10:
+
+

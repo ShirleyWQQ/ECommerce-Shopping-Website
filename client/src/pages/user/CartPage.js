f1be9c5d62aec760cd890ef4e94abf9b9cf16ec0
c684wang@uwaterloo.ca <c684wang@uwaterloo.ca>
2022-11-27 20:19:35 -0500

    cart page UI code

--- client/src/pages/user/CartPage.js
+++ client/src/pages/user/CartPage.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../stores/user";
+import ShoppingCart from "../../components/ShoppingCart";
 
-function CartPage() {
+// props: data(product[])
+function CartPage(props) {
   const user = useSelector(selectUser);
   if (user) {
     return (
       <div>
-        <h1>cart placeholder</h1>
+        <div>Shopping Cart</div>
+        <div className="flex-container" style={{ gap: "15px 20px" }}>
+          {props.data.map((item, index) => (
+            <ShoppingCart
+              key={index}
+              image={item.picture_source}
+              productName={item.product_name}
+              quantity={item.quantity}
+              price={item.price}
+            />
+          ))}
+        </div>
       </div>
     )
   } else {
     return (
       <div>
-        <h1>nothing</h1>
+        <h1>Sorry you need to login to view your cart :)</h1>
       </div>
     )
   }

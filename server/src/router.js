4691b7ce4676a68ba2d75cccf20913ff49c74910
Yunt Flare <peterliangap@outlook.com>
2022-11-28 19:13:04 -0500

    add comment endpoint

--- server/src/router.js
+++ server/src/router.js
@@ -13,7 +13,8 @@ router.delete("/product/:product_id", ProductController.deleteProductById);
 router.get("/product/:product_id/comments", CommentController.getByProductId);
 
 router.get("/user/:user_id/comments", CommentController.getByUserId);
-router.delete("/comment/:comment_id", CommentController.deleteById);
+router.delete("/comment/:comment_id", CommentController.deleteById);;
+router.post("/comment", CommentController.addComment);
 
 router.post("/user/login", UserController.login);
 router.post("/user/register", UserController.register);

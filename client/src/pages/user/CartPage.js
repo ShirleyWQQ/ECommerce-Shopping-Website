dc25467ec8993abdc9ccb2f9723a6954f5b43d40
Yunt Flare <peterliangap@outlook.com>
2022-11-29 18:41:53 -0500

    jump to login if not for cart

--- client/src/pages/user/CartPage.js
+++ client/src/pages/user/CartPage.js
@@ -1,43 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { selectUser } from "../../stores/user";
 import ShoppingCart from "../../components/ShoppingCart";
 import api from "../../lib/api";
 
-// props: data(product[])
-function CartPage(props) {
+// props:
+function CartPage() {
+  const navigate = useNavigate();
   const user = useSelector(selectUser);
 
   const [items, setItems] = useState([]);
   useEffect(() => {
-    api.getUserCart(user.user_id)
-      .then(setItems)
-      .catch(api.logError);
-  }, [user]);
-  if (user) {
-    return (
-      <div>
-        <div>Shopping Cart</div>
-        <div className="flex-container" style={{ gap: "15px 20px" }}>
-          {items.map((item, index) => (
-            <ShoppingCart
-              key={index}
-              image={item.picture_source}
-              productName={item.product_name}
-              quantity={item.quantity}
-              price={item.price}
-            />
-          ))}
-        </div>
+    if (user !== null) {
+      api.getUserCart(user.user_id)
+        .then(setItems)
+        .catch(api.logError);
+    } else {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <div className="flex-container" style={{ gap: "15px 20px" }}>
+        {items.map((item, index) => (
+          <ShoppingCart
+            key={index}
+            image={item.picture_source}
+            productName={item.product_name}
+            quantity={item.quantity}
+            price={item.price}
+          />
+        ))}
       </div>
-    )
-  } else {
-    return (
-      <div>
-        <h1>Sorry you need to login to view your cart :)</h1>
-      </div>
-    )
-  }
+    </div>
+  );
 }
 
 export default CartPage;

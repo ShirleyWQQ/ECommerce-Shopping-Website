6c5e4eac15668c2011391c5227c5ba4a00989e30
Yunt Flare <peterliangap@outlook.com>
2022-11-28 19:43:06 -0500

    update api

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -148,6 +148,14 @@ export default class Api {
       throw err;
     }
   }
+  static async getCartItem(userId, productId) {
+    try {
+      const res = await Axios.post(`${baseUrl}/comment`, {});
+      return res.data;
+    } catch (err) {
+      throw err;
+    }
+  }
   static logError(err) {
     alert(err);
   }

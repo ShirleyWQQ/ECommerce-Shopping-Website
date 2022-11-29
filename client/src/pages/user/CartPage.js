de94cf3a964fa3beb76a783397c06713a13fd83f
c684wang@uwaterloo.ca <c684wang@uwaterloo.ca>
2022-11-29 15:43:27 -0500

    code skeleton for CartPage

--- client/src/pages/user/CartPage.js
+++ client/src/pages/user/CartPage.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../stores/user";
 import ShoppingCart from "../../components/ShoppingCart";
+import Api from "../../lib/api";
 
 // props: data(product[])
 function CartPage(props) {
+  // states and varaibles
   const user = useSelector(selectUser);
+  const userId = user?.user_id;
+  const productName = props.productName;
+  const productId = props.productID;
+  const quantity = props.quantity;
+
+  // methods
+  const addToCart = useCallback(() => {
+    Api.addToCart(userId, productName);
+  }, [productName]);
+
+  const removeFromCart = useCallback(() => {
+    Api.removeFromCart(userId, productName);
+  }, [productName]);
+
+  const updateCart = useCallback(() => {
+    Api.updateCart(userId, productId, quantity);
+  }, [productId, quantity]);
+
+  const getCartItem = useCallback(() => {
+    Api.getCartItem(userId, productId);
+  }, [productId]);
+
+  const getCart = useCallback(() => {
+    Api.getCart(userId);
+  }, [userId]);
+
   if (user) {
     return (
       <div>
         <div>Shopping Cart</div>
+
         <div className="flex-container" style={{ gap: "15px 20px" }}>
           {props.data.map((item, index) => (
             <ShoppingCart

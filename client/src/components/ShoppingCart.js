03018f00287105ed54c4acf0f767712302f307cf
Xinyi Li <xinyili@v1040-wn-rt-b-49-195.campus-dynamic.uwaterloo.ca>
2022-11-30 15:18:26 -0500

    add item cart

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -31,6 +31,29 @@ export default function ShoppingCart(props) {
       });
   }
 
+  const addUpdateCart = () => {
+    const product_id = props.product_id;
+    const user_id = user?.user_id;
+    Api.getCartItem(user_id, product_id)
+      .then(res => {
+        if (res.length === 0) {
+          Api.addToCart(user_id, product_id.then(res1 => {
+            props.setItems(res1);
+          }).catch(err => alert("Add to shopping cart failed")));
+        } else {
+          Api.updateCart(user_id, product_id, res[0].quantity + 1).then(res1 => {
+            props.setItems(res1);
+          }).catch(err => alert("Add to shopping cart failed"));
+        }
+      })
+      .catch(err => {
+        if (err.response) {
+          alert("Login Failed");
+        } else
+          alert("Failed to retrieve products");
+      });
+  }
+
   return (
     <div className="container">
       <div className="row">
@@ -39,13 +62,14 @@ export default function ShoppingCart(props) {
         </div>
         <div className="col">
           <h5 className="productName">{props.productName}</h5>
-          <h5 className="quantity">{props.quantity}</h5>
         </div>
         <div className="col">
           <h5 className="price">${props.price}</h5>
         </div>
         <div className="col">
           <Button onClick={removeFromCart}>-</Button>
+          <h5 className="quantity">{props.quantity}</h5>
+          <Button onClick={addUpdateCart}>+</Button>
         </div>
       </div>
     </div>
